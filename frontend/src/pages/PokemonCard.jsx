dff05940297ce626ea30f7a865ca5d993dc1498d
nicolasdelisee <nicolas.delisee3@gmail.com>
2023-02-02 00:02:49 +0100

    Yup yup

--- frontend/src/pages/PokemonCard.jsx
+++ frontend/src/pages/PokemonCard.jsx
@@ -3,13 +3,14 @@
 // import PropTypes from "prop-types";
 import React from "react";
 import "./pokemonCard.scss";
-import plantCard from "../assets/pictures/card_plant.png";
+import fire from "../assets/pictures/cards/7.jpg";
 
 // eslint-disable-next-line react/prop-types
 function PokemonCard({ pokemon }) {
   return (
     <div className="PokemonCard">
-      <img src={plantCard} className="PokemonCard__emptyCard" alt="card" />
+      <img src={fire} className="PokemonCard__emptyCard" alt="card" />
+      <div className="PokemonCard__emptyCard" />
       <div className="PokemonCard__info">
         <h1 className="PokemonCard__info__id">ID {pokemon.id_pokedex}</h1>
         <h1 className="PokemonCard__info__name">{pokemon.name}</h1>
